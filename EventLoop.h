b14eda12026e5310a0f0a3ebcba10b28eb418bc6
scy_git <shuchunyu729@163.com>
2024-03-09 17:45:12 +0800

    add EventLoop

--- EventLoop.h
+++ EventLoop.h
@@ -7,6 +7,7 @@
 #include<mutex>
 
 #include "noncopyable.h"
+#include "CurrentThread.h"
 
 class Channel;
 class Poller;
@@ -24,11 +25,28 @@ public:
     //退出事件循环
     void quit();
 
+    Timestamp pollReturnTime() const {return pollReturnTime_;}
+
+    //在当前loop中执行cb
+    void runInLoop(Functor cb);
+    //把cb放入队列中，唤醒loop所在的线程，执行cb
+    void queueInLoop(Functor cb);
+
+    //唤醒loop所在的线程
+    void wakeup();
+
     //Eventloop 调用 poller
     void updateChannel(Channel* channel);
     void removeChannel(Channel* channel);
     bool hasChannel(Channel* channel);
+
+    //判断eventloop对象是否在自己的线程里面
+    bool isInLoopThread() const{return threadId_ == CurrentThread::tid();}
+
 private:
+    void handleRead();              //wake up
+    void doPendingFunctors();       //执行回调
+
     using ChannelList = std::vector<Channel*>;
 
     std::atomic_bool looping_;  //原子操作，通过CAS实现
@@ -42,7 +60,6 @@ private:
     std::unique_ptr<Channel> wakeupChannel_;
 
     ChannelList activeChannels_;
-    //Channel* currentActiveChannel_;
 
     std::atomic_bool callingPendingFunctors_;   //标识当前loop是否有需要执行的回调操作
     std::vector<Functor> pendingFunctors_;      //存储loop需要执行的所有回调操作
