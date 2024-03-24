b4ef115c83bbb0988fa68a67035e2d7f563ab4e0
scy_git <shuchunyu729@163.com>
2024-03-24 20:51:57 +0800

    update Buffer

---
+++ Buffer.cc
@@ -0,0 +1,47 @@
+#include "Buffer.h"
+
+#include<errno.h>
+#include<sys/uio.h>
+#include<unistd.h>
+
+//从fd上读取数据
+ssize_t Buffer::readFd(int fd, int* savedErrno)
+{
+    char extrabuf[65536] = {0};
+    struct iovec vec[2];
+    const size_t writeable = writableBytes();
+    vec[0].iov_base = begin() + writerIndex_;
+    vec[0].iov_len = writeable;
+    vec[1].iov_base = extrabuf;
+    vec[1].iov_len = sizeof extrabuf;
+
+    const int iovcnt = (writeable < sizeof extrabuf) ? 2 : 1;
+    const ssize_t n = readv(fd, vec, iovcnt);
+
+    if(n < 0)
+    {
+        *savedErrno = errno;
+    }
+    else if (n <= writeable)
+    {
+        writerIndex_ += n;
+    }
+    else
+    {
+        writerIndex_ = buffer_.size();
+        append(extrabuf, n - writeable);
+    }
+
+    return n;
+}
+
+ssize_t Buffer::writeFd(int fd, int* savedErrno)
+{
+    ssize_t n = ::write(fd, peek(), readableBytes());
+
+    if(n < 0)
+    {
+        *savedErrno = errno;
+    }
+    return n;
+}
