8fdfc119eb8c3b6a7a62fdff393cc462ed85a771
scy_git <shuchunyu729@163.com>
2024-03-28 16:03:27 +0800

    update

--- TcpServer.cc
+++ TcpServer.cc
@@ -35,7 +35,14 @@ TcpServer::TcpServer(EventLoop* loop, const InetAddress &listenAddr, const std::
 
 TcpServer::~TcpServer()
 {
-    
+    for(auto &item: connections_)
+    {
+        TcpConnectionPtr conn(item.second);
+        item.second.reset();
+
+        conn -> getLoop() -> runInLoop(
+            std::bind(&TcpConnection::connectDestoryed, conn));
+    }
 }
 
 void TcpServer::setThreadNum(int numThreads)
@@ -56,15 +63,56 @@ void TcpServer::start()
 //有新的连接，acceptor会执行这个回调
 void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr)
 {
+    EventLoop* ioloop = threadPool_ ->getNextLoop();
+    char buf[64] = {0};
+    snprintf(buf, sizeof buf, "-%s#%d", ipPort_.c_str(), nextConnId_);
+    ++nextConnId_;
+    std::string connName = name_ + buf;
+
+    LOG_INFO("TcpServer::newConnection [%s] - new connection [%s] from %s \n",
+        name_.c_str(), connName.c_str(), peerAddr.toIpPort().c_str());
+
+    //通过socket获取本机的ip地址和端口
+    sockaddr_in local;
+    ::bzero(&local, sizeof local);
+    socklen_t addrlen = sizeof local;
+    if(::getsockname(sockfd, (sockaddr*)&local, &addrlen) < 0)
+    {
+        LOG_ERROR("sockets::getlocalAddr");
+    }
+    InetAddress localAddr(local);
+
+    TcpConnectionPtr conn(new TcpConnection(
+                        ioloop,
+                        connName,
+                        sockfd,
+                        localAddr,
+                        peerAddr));
     
+    connections_[connName] = conn;
+    //用户传给TcpServer -> TcpConnection -> Channel -> Poller -> Channel回调
+    conn -> setConnectionCallback(connectionCallback_);
+    conn -> setMessageCallback(messageCallback_);
+    conn -> setWriteCompleteCallback(writeCompleteCallback_);
+    //conn -> shutdown
+    conn -> setCloseCallback(std::bind(&TcpServer::removeConnection, this, std::placeholders::_1));
+    
+    ioloop->runInLoop(std::bind(&TcpConnection::connectEstablished, conn));
 }
 
 void TcpServer::removeConnection(const TcpConnectionPtr& conn)
 {
-    
+    LOG_INFO("In %p TcpServer::removeConnection", loop_);
+    loop_->runInLoop(
+        std::bind(&TcpServer::removeConnectionInLoop, this, conn));
 }
 
 void TcpServer::removeConnectionInLoop(const TcpConnectionPtr &conn)
 {
-   
+   LOG_INFO("TcpServer::removeConnectionInLoop [%s] - connection %s\n"
+        ,name_.c_str(), conn->name().c_str());
+
+    connections_.erase(conn -> name());
+    EventLoop *ioloop = conn -> getLoop();
+    ioloop->queueInLoop(std::bind(&TcpConnection::connectDestoryed, conn));
 }
